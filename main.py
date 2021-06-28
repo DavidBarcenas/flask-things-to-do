8b0e0b7d53bcc9408bb1c566bf25f4094c945ff8
David Barcenas <daveepro@outlook.com>
2021-06-28 17:59:46 -0500

    install flask bootstrap and use sessions

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SECRET123'
 
 todos = ['Todo 1', 'Todo 2', 'Todo 3']
 
@@ -14,14 +18,16 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    # response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello_world():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip, 
         'todos': todos
